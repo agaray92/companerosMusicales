e826350dba96e43e2a2d4f6aa5fdc3f9499a9d39
Alexander Garay Pardo <agaray@softone.com.co>
2021-02-17 20:18:45 -0500

    Menu y footer dinamico

---
+++ js/menuFooter.js
@@ -0,0 +1,5 @@
+let menu = '<ul class="site-menu js-clone-nav d-none"><li><a href="index.html">Inicio</a></li><li><a href="about.html">Quienes somos</a></li><li class="has-children"><a href="episodes.html">Episodios</a><ul><li><a href="temporadaDos.html">Segunda Teporada</a></li><li><a href="temporadaUno.html">Primera Temporada</a></li></ul></li><li class="has-children"><a>Fotos</a><ul><li><a href="lanzamiento1.html">Fiesta Lanzamiento</a></li><li><a href="arkham1.html">Arkham</a></li><li><a href="casaAbuela1.html">Casa de la Abuela</a></li></ul></li><li><a href="videos.html">Videos</a></li><li><a href="contact.html">Contacto</a></li></ul>';
+document.getElementById('menuCompleto').innerHTML = menu;
+
+let footer = '<ul class="list-unstyled"><li class="mb-3"><a href="busStrike.html" class="d-flex"><figure class="image mr-4"><img src="images/episodio29.jpeg" alt="" class="img-fluid"></figure><div class="text"><h3 class="heading font-weight-light">Episodio 29: Bus Strike</h3></div></a></li></ul>';
+document.getElementById('footerUltimoEpisodio').innerHTML = footer;
